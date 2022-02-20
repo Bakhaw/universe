c7840964783c724ec77213328f2b5c359731e5b1
Hattou Faïssal <faissal.hattou@gmail.com>
2022-02-20 19:08:09 +0100

    Adjust ArchButton borders and Avatar has  better image quality

--- src/components/ArchButton/index.js
+++ src/components/ArchButton/index.js
@@ -2,7 +2,7 @@ import ArrowDownIcon from "../../assets/icons/arrow-down.svg";
 
 function ArchButton() {
   return (
-    <div className="border-4 border-very-dark-blue rounded-t-full bg-very-dark-blue-transparent">
+    <div className="border-4 border-b-0 border-very-dark-blue rounded-t-full bg-very-dark-blue-transparent">
       <a href="#about">
         <img alt="Arrow down" className="px-6 py-3" src={ArrowDownIcon} />
       </a>
